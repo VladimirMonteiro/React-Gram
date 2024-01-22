06b40b4dd9efcabd647f1d352f200b1f32d1f120
Vladimir Monteiro <117525954+VladimirMonteiro@users.noreply.github.com>
2024-01-22 16:21:45 -0300

    Update cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
 //cors
-app.use(cors({credentials: true, origin: "http://localhost:5174"}))
+app.use(cors({credentials: true, origin: "react-gram-mauve.vercel.app"}))
 
 //DB
 require('./db/conn')
